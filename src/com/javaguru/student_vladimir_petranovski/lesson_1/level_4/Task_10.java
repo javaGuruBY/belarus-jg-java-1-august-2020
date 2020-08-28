b7393dba7db16c95caba313222439a80321091b9
VladimirPetranovski <desantluninec@mail.ru>
2020-08-28 11:33:54 +0300

    vladimir_petranovski_lesson_1_level_4

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_1/level_4/Task_10.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_vladimir_petranovski.lesson_1.level_4;
+
+public class Task_10 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Vladimir");
+    }
+}
