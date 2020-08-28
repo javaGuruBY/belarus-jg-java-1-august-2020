4415a6e1d3ae10e0074dc2ca896431aa69c62f42
Vadim <vadimka193308@gmail.com>
2020-08-28 13:00:13 +0300

    Homework

---
+++ src/com/javaguru/student_vadim_migun/lesson2/level2/Task7.java
@@ -0,0 +1,27 @@
+package com.javaguru.student_vadim_migun.lesson2.level2;
+
+// отсутсвует static
+
+class LoginService {
+
+    public static void main (String [] args) {
+    System.out.println("You can login!");
+
+    }
+
+}
+
+
+
+/*
+Дан код, нужно сделать этот код запускаемым.
+
+class LoginService {
+
+	public void main(String[] args) {
+		System.out.println("You can login!");
+	}
+
+
+}
+ */
