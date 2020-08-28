6594906e0891ffd3104fcbaa1a79c15ffc56c735
Vadim <vadimka193308@gmail.com>
2020-08-28 12:49:26 +0300

    Homework

---
+++ src/com/javaguru/student_vadim_migun/lesson2/level2/Phone.java
@@ -0,0 +1,17 @@
+package com.javaguru.student_vadim_migun.lesson2.level2;
+
+class Phone {
+    public static void main(String [] args){
+
+    }
+}
+
+
+/*
+Дан код, нужно исправить ошибку компиляции.
+
+class Phone
+
+}
+
+ */
