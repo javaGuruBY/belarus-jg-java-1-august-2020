64b54df1a91327634f0eaa3a53c1d050c579eba4
Vadim <vadimka193308@gmail.com>
2020-08-28 12:35:38 +0300

    Homework

---
+++ src/com/javaguru/student_vadim_migun/lesson2/leve1/Task2.java
@@ -0,0 +1,36 @@
+package com.javaguru.student_vadim_migun.lesson2.leve1;
+
+
+import java.util.Scanner;
+
+class Task2 {
+
+    public static void main (String [] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter first fractional number: ");
+        double firstNumber = sc.nextDouble();
+        System.out.println("Fractional first number = " + firstNumber);
+
+        System.out.println("Please enter second fractional number: ");
+        double secondNumber = sc.nextDouble();
+        System.out.println("Fractional second number: " + secondNumber);
+
+        double summResult = firstNumber + secondNumber;
+        System.out.println("Summ result = " + summResult);
+
+        double subtractResult = firstNumber - secondNumber;
+        System.out.println("Subtract result = " + subtractResult);
+
+        double multiplyResult = firstNumber * secondNumber;
+        System.out.println("Multiply result = " + multiplyResult);
+
+        double divideResult = firstNumber / secondNumber;
+        System.out.println("Divide result = " + divideResult);
+
+
+
+    }
+
+}
