089d0902cb2a213d7a4428a915ac99cf75cc0379
Vadim <vadimka193308@gmail.com>
2020-08-28 12:52:05 +0300

    Homework

---
+++ src/com/javaguru/student_vadim_migun/lesson2/level2/BankAccount.java
@@ -0,0 +1,22 @@
+package com.javaguru.student_vadim_migun.lesson2.level2;
+
+// отсутствует }
+
+class BankAccount {
+
+    public static void main (String [] args) {
+
+    }
+}
+
+
+
+/*
+Дан код, нужно исправить ошибку компиляции.
+
+class BankAccount {
+
+	public static void main(String[] args) {
+
+}
+ */
