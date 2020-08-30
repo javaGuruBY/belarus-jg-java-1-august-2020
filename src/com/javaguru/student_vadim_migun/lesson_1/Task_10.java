18dbeaaf521400ff82437ba69a872644438849c3
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-08-30 20:32:08 +0300

    Task_10.java

--- src/com/javaguru/student_vadim_migun/lesson_1/Task_10.java
+++ src/com/javaguru/student_vadim_migun/lesson_1/Task_10.java
@@ -2,9 +2,9 @@ package com.javaguru.student_vadim_migun.lesson_1;
 
 public class Task_10 {
 
-    public static void main (String [] args)
-    {
-        System.out.println ("Hello World!");
+    public static void main (String [] args) {
+        
+        System.out.println("Hello World!");
         System.out.println("Vadim");
     }
 
