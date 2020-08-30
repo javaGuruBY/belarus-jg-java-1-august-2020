b3d07dc3e2ed94824ccbb7986a5645de58dabc84
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-08-30 21:33:36 +0300

    Update Task_10_RadiusCircle.java

--- src/com/javaguru/student_vladimir_petranovski/lesson_2/level_3/Task_10_RadiusCircle.java
+++ src/com/javaguru/student_vladimir_petranovski/lesson_2/level_3/Task_10_RadiusCircle.java
@@ -1,15 +1,15 @@
 package com.javaguru.student_vladimir_petranovski.lesson_2.level_3;
 
-import java.util.Locale;
+import java.util.Locale; //Зачем он здесь нужен?
 import java.util.Scanner;
 
 public class Task_10_RadiusCircle {
     public static void main(String[] args) {
         System.out.println("Введите радиус круга (double):");
         Scanner sc = new Scanner(System.in);
-        sc.useLocale(Locale.US);
+        sc.useLocale(Locale.US); //Не вижу смысле использовать этот Класс и его метод
         double radius = sc.nextDouble();
-        final double PI = 3.1415;
+        final double PI = 3.1415; // Лучше использовать готовый класс и его переменную import static java.lang.Math.PI
         System.out.println("Test Data:");
         System.out.println("Radius = " + radius);
         System.out.println("Expected Output");
