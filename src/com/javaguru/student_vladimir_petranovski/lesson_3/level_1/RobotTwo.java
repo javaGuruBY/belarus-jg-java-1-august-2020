a1bb39111a2abff7770dcbdc0294a13517a61e37
VladimirPetranovski <desantluninec@mail.ru>
2020-09-01 08:49:56 +0300

    vladimir_petranovski_lesson_3_level_1_Task_2

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_3/level_1/RobotTwo.java
@@ -0,0 +1,14 @@
+package com.javaguru.student_vladimir_petranovski.lesson_3.level_1;
+/*
+В классе робот (Robot) создайте конструктор без параметров:
+
+    public Robot() {
+
+    }
+ */
+
+public class RobotTwo {
+    public RobotTwo() {
+
+    }
+}
