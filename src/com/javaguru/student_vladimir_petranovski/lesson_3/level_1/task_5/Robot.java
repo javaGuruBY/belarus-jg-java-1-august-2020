4a7aad7ef2e0e8b3ef55229283db4c2a609f400c
VladimirPetranovski <desantluninec@mail.ru>
2020-09-01 10:00:28 +0300

    petranovski/lesson_3/level_1/task_5

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_3/level_1/task_5/Robot.java
@@ -0,0 +1,21 @@
+package com.javaguru.student_vladimir_petranovski.lesson_3.level_1.task_5;
+/*
+Научите вашего робота "говорить" как его зовут.
+
+Создайте в классе Robot метод со следующей сигнатурой:
+
+    public void sayYourName() {
+        // тут напишите команду распечатки на консоли строки "My name is ROBO"
+    }
+
+В методе main(...) класса RobotDemo вызовите
+метод sayYourName() у созданного ранее виртуального обьекта.
+Запустите вашу программу и убедитесь, что созданный вами
+виртуальный обьект робот напечатал на консоли "My name is ROBO".
+ */
+
+public class Robot {
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+}
