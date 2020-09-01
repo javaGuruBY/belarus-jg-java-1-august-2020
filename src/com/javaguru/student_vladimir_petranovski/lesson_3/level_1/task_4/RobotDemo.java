2c145e41c196a589855ca35f4573f5579d0e10f2
VladimirPetranovski <desantluninec@mail.ru>
2020-09-01 09:53:43 +0300

    vladimir_petranovski_lesson_3_level_1_Task_4

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_3/level_1/task_4/RobotDemo.java
@@ -0,0 +1,8 @@
+package com.javaguru.student_vladimir_petranovski.lesson_3.level_1.task_4;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot rob = new Robot();
+        rob.sayHello();
+    }
+}
