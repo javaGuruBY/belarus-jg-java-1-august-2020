c2143f9095291e633a2e4803a3c301ec17825c16
Vadim <vadimka193308@gmail.com>
2020-09-04 14:15:32 +0300

    Homework

---
+++ src/com/javaguru/student_vadim_migun/lesson_3/level_1/Robot.java
@@ -0,0 +1,34 @@
+package com.javaguru.student_vadim_migun.lesson_3.level_1;
+
+/*
+Создайте класс робот (Robot).
+
+Научите вашего робота "говорить" привет.
+
+Создайте в классе Robot метод со следующей сигнатурой:
+
+    public void sayHello() {
+        // тут напишите команду распечатки на консоли строки "Hello!"
+    }
+
+ */
+
+ class Robot {
+
+     public Robot () {
+     }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO1");
+    }
+
+    public void sayYourName2() {
+        System.out.println("My name is ROBO2");
+
+    }
+
+}
---
+++ src/com/javaguru/student_vadim_migun/training/Book.java
@@ -0,0 +1,21 @@
+package com.javaguru.student_vadim_migun.training;
+
+public class Book {
+
+    String title;
+    int pageCount;
+    boolean opened;
+
+    void open() {
+        opened = true;
+    }
+
+    void close() {
+        opened = false;
+    }
+
+    boolean isOpened() {
+        return opened;
+    }
+
+}
