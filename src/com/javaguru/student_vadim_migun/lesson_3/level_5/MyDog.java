c2ad3e4197790ac3b6ac1800db7a73fd2043b860
Vadim <vadimka193308@gmail.com>
2020-09-08 21:18:00 +0300

    Homework

--- src/com/javaguru/student_vadim_migun/lesson_3/level_5/MyDog.java
+++ src/com/javaguru/student_vadim_migun/lesson_3/level_5/MyDog.java
@@ -16,13 +16,12 @@ package com.javaguru.student_vadim_migun.lesson_3.level_5;
  */
 
 
-class DogDemo {
+class MyDogDemo {
     public static void main(String[] args) {
-        Dog myDog = new Dog();
-        myDog.setName("Rex", 5, "black");
+        MyDog myDog = new MyDog("Rex",5, "black");
         myDog.voice();
         myDog.happyBirthday();
-        myDog.changeColor("white");
+        myDog.changeColor1 ("White");
         myDog.changeName("Sharik");
 
     }
@@ -30,19 +29,21 @@ class DogDemo {
 }
 
 
-class Dog {
+class MyDog {
 
     private String name;
     private int age;
     private String color;
-    private String newColor;
-    private String newName;
+    // private String newColor;
+    // private String newName;
 
-    public void setName(String name, int age, String color){
+    public MyDog (String name, int age, String color){
         this.name = name;
         this.age = age;
         this.color = color;
+
     }
+
     public void voice(){
         for(int i=0; i<3; i++){
         System.out.println(name + ", " + age + " year, " + color);
@@ -54,13 +55,13 @@ class Dog {
         System.out.println("HappyBirthday: " + age + " year ");
     }
 
-    public void changeColor(String newColor) { //public void changeColor(String newColor) {
-        this.newColor = newColor;// напишите тут команду сохранения нового цвета в свойство класса
+    public void changeColor1 (String newColor) {    //public void changeColor(String newColor) {
+        this.color = newColor;   // напишите тут команду сохранения нового цвета в свойство класса
         System.out.println("Change color: " + newColor);
     }
 
-    public void changeName(String newName) { // дополнительная команда от себя
-        this.newName = newName;
+    public void changeName(String newName) {   // дополнительная команда от себя
+        this.name = newName;
         System.out.println("New name: " + newName);
     }
 
