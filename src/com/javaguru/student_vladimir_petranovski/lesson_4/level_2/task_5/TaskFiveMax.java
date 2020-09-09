fc4e38dd32f697b6f8bae622ec3ab39fea226c21
VladimirPetranovski <desantluninec@mail.ru>
2020-09-09 10:52:28 +0300

    petranovski_lesson_4_level_2_task_5

--- src/com/javaguru/student_vladimir_petranovski/lesson_4/level_2/task_5/TaskFiveMax.java
+++ src/com/javaguru/student_vladimir_petranovski/lesson_4/level_2/task_5/TaskFiveMax.java
@@ -1,6 +1,11 @@
 package com.javaguru.student_vladimir_petranovski.lesson_4.level_2.task_5;
 
 import java.util.Scanner;
+/*
+Написать программу, которая запрашивает у пользователя
+два целых числа и выводит на консоль наибольшее из них.
+Нельзя пользоваться классом Math, можно использовать только if else.
+ */
 
 public class TaskFiveMax {
     public static void main(String[] args) {
