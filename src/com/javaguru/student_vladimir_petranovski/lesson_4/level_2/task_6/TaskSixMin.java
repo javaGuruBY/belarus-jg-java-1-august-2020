a10cd2bd3c74cf950de408d26744039ff1cb909d
VladimirPetranovski <desantluninec@mail.ru>
2020-09-09 10:42:01 +0300

    petranovski_lesson_4_level_2_task_6

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_4/level_2/task_6/TaskSixMin.java
@@ -0,0 +1,20 @@
+package com.javaguru.student_vladimir_petranovski.lesson_4.level_2.task_6;
+
+import java.util.Scanner;
+
+public class TaskSixMin {
+    public static void main(String[] args) {
+        System.out.println("Please enter an integer:");
+        Scanner sc = new Scanner(System.in);
+        int firstNumber = sc.nextInt();
+        System.out.println("Please enter an integer:");
+        int secondNumber = sc.nextInt();
+        if(firstNumber > secondNumber) {
+            System.out.println("Min integer - second number");
+        } else if(firstNumber < secondNumber) {
+            System.out.println("Min integer - first number");
+        } else {
+            System.out.println("Min integer - first number");
+        }
+    }
+}
