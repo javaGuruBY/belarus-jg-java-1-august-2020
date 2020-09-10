f1c3aadb420aed219427523e3f79c29fa5191f9d
spierok <spier@inbox.ru>
2020-09-10 20:17:53 +0300

    lesson 3, level 1

---
+++ src/com/javaguru/student_Pavel_Buben/lesson_3/level_1/Robot.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_Pavel_Buben.lesson_3.level_1;
+
+class Robot {
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+        public Robot() {
+
+    }
+    }
+
