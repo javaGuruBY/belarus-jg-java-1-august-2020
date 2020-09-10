d45ddb43c0182ce5a2e0f3d8765fae764ca3abb1
shldenis <1976denis37@gmail.com>
2020-09-10 18:39:27 +0300

    lesson 4 up to task15

--- src/com/javaguru/student_Denis_Shlichkov/lesson_4/level_1_intern/TaskThree.java
+++ src/com/javaguru/student_Denis_Shlichkov/lesson_4/level_1_intern/TaskThree.java
@@ -14,19 +14,34 @@ package lesson_4.level_1_intern;
 
 import java.util.Scanner;
 
-public class TaskThree {
+class TaskThree {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int dayWeek = scanner.nextInt();
         switch (dayWeek) {
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
-            default -> System.out.println("Is not a day week");
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Is not a day week");
         }
     }
 }
