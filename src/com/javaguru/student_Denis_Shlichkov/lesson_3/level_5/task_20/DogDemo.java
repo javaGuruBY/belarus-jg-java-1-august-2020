e9933919b08a75dc8601b67d6aefd34762e593ad
spierok <spier@inbox.ru>
2020-09-11 23:30:17 +0300

    lesson_3, level_5

--- src/com/javaguru/student_Denis_Shlichkov/lesson_3/level_5/task_20/DogDemo.java
+++ src/com/javaguru/student_Denis_Shlichkov/lesson_3/level_5/task_20/DogDemo.java
@@ -1,7 +1,7 @@
 package lesson_3.level_5.task_20;
 
 
-public class DogDemo {
+class DogDemo {
     public static void main(String[] args) {
         Dog firstDog = new Dog("Bobby", 5);
         System.out.println("Dog was created with name " + firstDog.getDogName() +
