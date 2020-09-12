938839a4f853d102774f67df3a6e06d03b0c490b
Vadim <vadimka193308@gmail.com>
2020-09-13 00:32:56 +0300

    Homework

--- src/com/javaguru/student_vadim_migun/lesson_4/level_1_intern/PositiveAndNegative.java
+++ src/com/javaguru/student_vadim_migun/lesson_4/level_1_intern/PositiveAndNegative.java
@@ -1,21 +1,25 @@
-package com.javaguru.student_vadim_migun.lesson_4.Level_1;
-
-import java.util.Scanner;
-
+package com.javaguru.student_vadim_migun.lesson_4.level_1_intern;
 /*
 Написать программу, которая запрашивает у
 пользователя
 целое число и выводит на консоль
 положительное оно или отрицательное.
  */
-class IntegerNumber {
+import java.util.Scanner;
+
+class PositiveAndNegative {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Input integer number: ");
-        int firstNumber = scan.nextInt();
-        System.out.println("Integer number = " + firstNumber);
+        int number = scan.nextInt();
+        System.out.println("Number = " + number);
 
+        if (number > 0){
+            System.out.println("the number is positive");
+        }else {
+            System.out.println("the number is negative");
 
+        }
     }
 
 }
