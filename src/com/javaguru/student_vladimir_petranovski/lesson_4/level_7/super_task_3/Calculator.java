2686ec3f763c116348f7cc62f6ce60ef1c8e1cc9
VladimirPetranovski <desantluninec@mail.ru>
2020-09-12 17:25:00 +0300

    petranovski_lesson_4_level_7_super_task_3

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_4/level_7/super_task_3/Calculator.java
@@ -0,0 +1,23 @@
+package com.javaguru.student_vladimir_petranovski.lesson_4.level_7.super_task_3;
+
+class Calculator {
+    public int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int sub(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int mul(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int div(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
