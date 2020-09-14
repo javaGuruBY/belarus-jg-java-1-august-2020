4d6ce08142edba65754059f09c2d45dc344eedcc
ArtikTico <58955407+ArtikTico@users.noreply.github.com>
2020-09-14 21:03:39 +0300

    Update ArrayDefinition5.java

--- src/com/javaguru/student_Denis_Shlichkov/lesson_5/level_1_intern/taskFive/ArrayDefinition5.java
+++ src/com/javaguru/student_Denis_Shlichkov/lesson_5/level_1_intern/taskFive/ArrayDefinition5.java
@@ -1,7 +1,9 @@
 package lesson_5.level_1_intern.taskFive;
 
 public class ArrayDefinition5 {
-
+/*Проверено Артемом:
+Все гораздо проще можно сделать.
+int [] numbers = {1,2,3,4};*/
     public static void main(String[] args) {
         int [] numbers = new int[5];
         numbers = new int[]{1, 2, 3, 4, };
