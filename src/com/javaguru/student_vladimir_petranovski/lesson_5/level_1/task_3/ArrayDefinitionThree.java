1276bee16837bb5c1d0ff74cf467cd40fb070c98
VladimirPetranovski <desantluninec@mail.ru>
2020-09-15 09:02:25 +0300

    vladimir_petranovski_lesson_5_level_1_task_3

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_5/level_1/task_3/ArrayDefinitionThree.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_vladimir_petranovski.lesson_5.level_1.task_3;
+/*
+Найдите ошибку в программе и исправить её.
+В переменная numbers должна ссылаться на массив целых чисел.
+class ArrayDefinition3 {
+    public static void main(String[] args) {
+        boolean numbers = new int[10];
+    }
+}
+ */
+class ArrayDefinitionThree {
+    public static void main(String[] args) {
+        boolean[] numbers = new boolean[10];
+    }
+}
