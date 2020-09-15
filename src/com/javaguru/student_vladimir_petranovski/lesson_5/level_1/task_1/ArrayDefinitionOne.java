1b1029d1a28a2160f276f44f18a69ba4d95b98b5
VladimirPetranovski <desantluninec@mail.ru>
2020-09-15 08:57:19 +0300

    vladimir_petranovski_lesson_5_level_1_task_1

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_5/level_1/task_1/ArrayDefinitionOne.java
@@ -0,0 +1,15 @@
+package com.javaguru.student_vladimir_petranovski.lesson_5.level_1.task_1;
+/*
+Найдите ошибку в программе и исправить её:
+class ArrayDefinition1 {
+    public static void main(String[] args) {
+        int numbers = new int[];
+    }
+}
+ */
+
+class ArrayDefinitionOne {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+    }
+}
