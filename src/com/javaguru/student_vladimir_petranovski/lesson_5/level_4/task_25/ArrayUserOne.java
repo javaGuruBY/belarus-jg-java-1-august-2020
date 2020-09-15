a0430d7925f79bc2d67070692f76d7626491bd8b
VladimirPetranovski <desantluninec@mail.ru>
2020-09-15 12:22:19 +0300

    vladimir_petranovski_lesson_5_level_4_task_25

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_5/level_4/task_25/ArrayUserOne.java
@@ -0,0 +1,26 @@
+package com.javaguru.student_vladimir_petranovski.lesson_5.level_4.task_25;
+
+import java.util.Scanner;
+
+/*
+Напишите программу, в которой:
+- запросите у пользователя с консоли длину массива
+- создайте массив указанной пользователем длины
+- заполните массив числами полученными от пользователя
+- распечатайте на консоль все элементы массива.
+ */
+class ArrayUserOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length array: ");
+         double[] arr = new double[sc.nextInt()];
+        System.out.println("Enter numbers double: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextDouble();
+        }
+        for (double i : arr) {
+            System.out.println(i);
+        }
+
+    }
+}
