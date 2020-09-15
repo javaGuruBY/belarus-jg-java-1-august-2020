27f18e79c8362e5d1d6b2793d16d528cc3076a04
VladimirPetranovski <desantluninec@mail.ru>
2020-09-15 08:59:56 +0300

    vladimir_petranovski_lesson_5_level_1_task_2

---
+++ src/com/javaguru/student_vladimir_petranovski/lesson_5/level_1/task_2/ArrayDefinitionTwo.java
@@ -0,0 +1,14 @@
+package com.javaguru.student_vladimir_petranovski.lesson_5.level_1.task_2;
+/*
+Найдите ошибку в программе и исправить её:
+class ArrayDefinition2 {
+    public static void main(String[] args) {
+        int numbers = new byte[10];
+    }
+}
+ */
+class ArrayDefinitionTwo {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
+}
