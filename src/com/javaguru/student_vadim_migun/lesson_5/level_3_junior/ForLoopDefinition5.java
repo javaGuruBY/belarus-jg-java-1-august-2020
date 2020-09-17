840da0a26f5c6724bf6fbc4d5320e5542d67cf4f
Vadim <vadimka193308@gmail.com>
2020-09-17 17:57:45 +0300

    Homework

---
+++ src/com/javaguru/student_vadim_migun/lesson_5/level_3_junior/ForLoopDefinition5.java
@@ -0,0 +1,48 @@
+package com.javaguru.student_vadim_migun.lesson_5.level_3_junior;
+/*
+Найдите логическую ошибку в программе и исправить её:
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++ {
+            System.out.println(i);
+        }
+    }
+
+}
+ */
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+/*
+Найдите логическую ошибку в программе и исправить её:
+
+class ForLoopDefinition5 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
+        }
+    }
+
+}
+
+ */
+class ForLoopDefinition5_1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+    }
+}
+
+}
+
