1877e314a6ee7fb07e11d38a0fadc29838d3af31
Sergey Kazyanin <sergey.miha@gmail.com>
2020-09-22 14:13:08 +0300

    add lesson_6 | level_INTERN

---
+++ src/com/javaguru/student_sergey_kazyanin/lesson_6/level_1_intern/Task_1/Calculator.java
@@ -0,0 +1,4 @@
+package com.javaguru.student_sergey_kazyanin.lesson_6.level_1_intern.Task_1;
+
+class Calculator {
+}
---
+++ src/com/javaguru/student_sergey_kazyanin/lesson_6/level_1_intern/Task_3/FizzBuzzTest.java
@@ -0,0 +1,4 @@
+package com.javaguru.student_sergey_kazyanin.lesson_6.level_1_intern.Task_3;
+
+class FizzBuzzTest {
+}
