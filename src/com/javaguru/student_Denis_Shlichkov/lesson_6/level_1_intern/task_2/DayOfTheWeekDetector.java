ca774f1f5e55c5800fba3e470ba646ee94f09645
Denis76 <shldenis@yandex.ru>
2020-09-23 18:12:17 +0300

    Lesson 6 Task_3

---
+++ src/com/javaguru/student_Denis_Shlichkov/lesson_6/level_1_intern/task_2/DayOfTheWeekDetector.java
@@ -0,0 +1,22 @@
+package lesson_6.level_1_intern.task_2;
+
+class DayOfTheWeekDetector {
+    String detectDayName(int number){
+        java.lang.String dayWeek = null;
+        if ((number >=1) && (number <= 7)) {
+            switch (number) {
+                case 1 -> dayWeek = "Monday";
+                case 2 -> dayWeek = "Tuesday";
+                case 3 -> dayWeek = "Wednesday";
+                case 4 -> dayWeek = "Thursday";
+                case 5 -> dayWeek = "Friday";
+                case 6 -> dayWeek = "Saturday";
+                case 7 -> dayWeek = "Sunday";
+            }
+        }
+        else {
+            dayWeek = "Please input a valid number between 1 and 7";
+        }
+        return dayWeek;
+    }
+}
