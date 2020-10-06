748734c7474860bf91b32e935ba1ed134eb0ae43
VladimirPetranovski <desantluninec@mail.ru>
2020-10-06 15:49:58 +0300

    vladimir_petranovski_esson_4_level _7_.=super_task_4_junit_test

---
+++ src/test/java/com/javaguru/student_vladimir_petranovski/lesson_4/level_7/super_task_4/LightColorDetectorTest.java
@@ -0,0 +1,12 @@
+package com.javaguru.student_vladimir_petranovski.lesson_4.level_7.super_task_4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LightColorDetectorTest {
+
+    @Test
+    public void detect() {
+    }
+}
