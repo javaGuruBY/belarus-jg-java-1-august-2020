b5ff3f0ec6478a381504a103e7afee27f3508477
VladimirPetranovski <desantluninec@mail.ru>
2020-10-08 09:42:58 +0300

    vladimir_petranovski_changed_tests_lesson_4_level_7_super_task_4

--- src/test/java/com/javaguru/student_vladimir_petranovski/lesson_4/level_7/super_task_4/LightColorDetectorTest.java
+++ src/test/java/com/javaguru/student_vladimir_petranovski/lesson_4/level_7/super_task_4/LightColorDetectorTest.java
@@ -1,12 +1,57 @@
 package com.javaguru.student_vladimir_petranovski.lesson_4.level_7.super_task_4;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LightColorDetectorTest {
+    private LightColorDetector lightColorDetector;
+
+    @Before
+    public void setUp() {
+        lightColorDetector = new LightColorDetector();
+    }
 
     @Test
-    public void detect() {
+    public void detectTestOne() {
+        String actual = lightColorDetector.detect(381);
+        assertEquals("Violet", actual);
+    }
+
+    @Test
+    public void detectTestTwo() {
+        String actual = lightColorDetector.detect(463);
+        assertEquals("Blue", actual);
+    }
+
+    @Test
+    public void detectTestThree() {
+        String actual = lightColorDetector.detect(503);
+        assertEquals("Green", actual);
+    }
+
+    @Test
+    public void detectTestFour() {
+        String actual = lightColorDetector.detect(575);
+        assertEquals("Yellow", actual);
+    }
+
+    @Test
+    public void detectTestFive() {
+        String actual = lightColorDetector.detect(591);
+        assertEquals("Orange", actual);
+    }
+
+    @Test
+    public void detectTestSix() {
+        String actual = lightColorDetector.detect(740);
+        assertEquals("Red", actual);
+    }
+
+    @Test
+    public void detectTestSeven() {
+        String actual = lightColorDetector.detect(795);
+        assertEquals("Invisible Light", actual);
     }
 }
