ff3f4940f012fd643e495078b3e5b5823548be4e
Denis76 <shldenis@yandex.ru>
2020-10-27 01:47:53 +0300

    Lesson 6 solved to Task_27

---
+++ src/main/java/com/javaguru/student_Denis_Shlichkov/lesson_7/level_1_intern/task_1/WordService.java
@@ -0,0 +1,11 @@
+package com.javaguru.student_Denis_Shlichkov.lesson_7.level_1_intern.task_1;
+
+class WordService {
+    String findMostFrequentWord(String text) {
+        return "";
+    }
+
+    String[] removePunctuationMarks(String text){
+        return text.split("\\.?,?\\s");
+    }
+}
